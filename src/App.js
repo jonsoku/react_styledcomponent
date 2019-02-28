fa7e4053501d66bab6ccb0741bb27b37f73bae61
Jongseok Lee <jongseoklee@Jongseokui-iMac.local>
2019-02-28 12:16:09 +0900

    theme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react';
-import styled, { createGlobalStyle, css , keyframes } from "styled-components";
-
+import styled, { createGlobalStyle, css , keyframes, ThemeProvider} from "styled-components";
+import theme from "./theme"
 /* 기존
 
 
@@ -18,86 +18,47 @@ const GlobalStyle = createGlobalStyle`
 `
 
 //그룹화
-const Card = css`
-  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0,0,0,0.08);
-  background-color: white;
-  border-radius: 10px;
-  padding:20px;
-`
-
+const Card = styled.div`
+  background-color: red;
+`;
 
 const Container = styled.div`
   height: 100vh;
-  width:100%;
-  background-color: grey;
-`;
-
-/* styled-components적용 */
-const Button = styled.button`
-  border-radius: 50px;
-  padding:5px;
-  min-width:120px;
-  color:white;
-  font-weight: 600;
-  -webkit-appearance: none;
-  cursor: pointer;
-  &:active,
-  &:focus{
-    outline:none;
+  width: 100%;
+  background-color: pink;
+  ${Card} {
+    background-color: blue;
   }
-  background-color: ${props => props.danger ? "#e74c3c" : "skyblue"};
-  
-  ${props => {
-    if (props.danger) {
-        return css`animation: ${Rotation} ${props.rotationTime}s linear infinite;`;
-      }
-  }}
-  
-  
-  
 `;
 
-//똑같은 스타일(css)를 다른 태그사용할때 재활용하기.
-//const Anchor = Button.withComponent("a")
-//const Anchor = Button.withComponent("a").extend`
-// text-decoration:none;
-// `;
-
-const Anchor = styled(Button.withComponent("a"))`
-  text-decoration:none;
+const Button = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${props => props.theme.successColor};
 `;
-
-
-const Rotation = keyframes`
-from{
-transform: rotate(0deg);
-}
-to{
-transform: rotate(360deg);
-}
-`;
-
-
-const Input = styled.input.attrs({
-    required: true
-})`
-  border:none;
-  ${Card}
+const Button2 = styled.button`
+  border-radius: 30px;
+  padding: 25px 15px;
+  background-color: ${props => props.theme.dangerColor};
 `;
 
 class App extends Component {
-  render() {
-    return (
-        <>
-        <GlobalStyle/>
-        <Container>
-            <Input placeholder="hello"/>
-        </Container>
-        </>
-    );
-  }
+    render() {
+        return (
+            <ThemeProvider theme={theme}>
+                <Container>
+                    <Form />
+                </Container>
+            </ThemeProvider>
+        );
+    }
 }
 
-
+const Form = () => (
+    <Card>
+        <Button>Success</Button>
+        <Button2>Danger</Button2>
+    </Card>
+);
 
 export default App;
