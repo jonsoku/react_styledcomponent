fbe3b2a0fff307f7d8616aff9430e9b7947ecf54
Jongseok Lee <jongseoklee@Jongseokui-iMac.local>
2019-02-28 11:31:59 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+        <>
+          <Button danger/>
+          <Button />
+        </>
     );
   }
 }
+
+const Button = ({danger}) => (
+    <button className={danger ? "button button--danger" : "button button--success"}>Hello</button>
+)
+
 
 export default App;
